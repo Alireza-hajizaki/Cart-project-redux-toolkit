34b3b614adcae8767ff4b39edebff86e0eb68cbf
Shahram-hajizaki <shahramhjz75@gmail.com>
2023-07-06 09:32:49 +0330

    strucure created

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import NavbarWithDropdown from "./components/Navbar/Index";
+import ECommerceCard from "./components/Cart/Cart";
+
+function App() {
+  return (
+        <>
+            <NavbarWithDropdown/>
+            <ECommerceCard />
+        </>
+
+);
+}
+
+export default App;
